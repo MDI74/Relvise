0715d858fa23c501f47b0a1c2e423db16d7a1a4f
MDI74 <demon4301@gmail.com>
2022-12-03 14:25:00 +0500

    add menu-burger, main-block
    add menu-burger, main-block, these blocks are adapted

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const menuBurger = document.querySelector('.header__burger');
-
-console.log(menuBurger);
+const navMenu = document.querySelector('.header__menu ');
 
 menuBurger.addEventListener('click', function(){
-    menuBurger.classList.toggle('active')
+    menuBurger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+
 })
+
+navMenu.addEventListener('click', function () {
+    const headerMenu = this.closest('.header__menu ');
+    headerMenu.classList.remove('active');
+    menuBurger.classList.remove('active');
+})
+
+
