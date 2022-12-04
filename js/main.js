b7817ad732c094fbf91aa9ef187a52a141831111
MDI74 <demon4301@gmail.com>
2022-12-04 22:32:47 +0500

    add sections practice, about, clients, subscribe,contact
    add sections practice, about, clients, subscribe,contact they are all adapted

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const menuBurger = document.querySelector('.header__burger');
-const navMenu = document.querySelector('.header__menu ');
+const navMenu = document.querySelector('.header__menu');
+const columnContacts = document.querySelectorAll('[data-contact]')
+const itemsContact = document.querySelectorAll('.item-contact')
 
 menuBurger.addEventListener('click', function(){
     if(menuBurger.classList.toggle('active')){
@@ -10,16 +12,26 @@ menuBurger.addEventListener('click', function(){
     }
     navMenu.classList.toggle('active');
 
-    
-
 })
 
 navMenu.addEventListener('click', function () {
-    const headerMenu = this.closest('.header__menu ');
+    const headerMenu = this.closest('.header__menu');
     headerMenu.classList.remove('active');
     menuBurger.classList.remove('active');
-    menuBurger.style.cssText= ""
+    menuBurger.style.cssText= "";
+})
 
+columnContacts.forEach(function(item){
+    item.addEventListener('click', function(){
+        const contact = document.querySelector('#' + this.dataset.contact);
+
+        itemsContact.forEach(function (item) { 
+            item.classList.remove('active');
+       })
+
+        contact.classList.toggle('active');
+
+    })
 
 })
 
